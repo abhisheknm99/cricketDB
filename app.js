99ebe92c108eb44d80d8aa7d8877c3038cb95c16
Abhishek <abhisheknm99@gmail.com>
2020-04-19 20:48:20 +0530

    abhishek

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/crickdb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected',()=>{
+	console.log("Connected to db ");
+});
+
+mongoose.connection.on('error',(err)=>{
+	console.log("Error connecting to db"+err);
+});
+
+const app = express();
+
+const players = require('./routes/players');
+const teams = require('./routes/teams');
+
+const port = process.env.PORT || 3030;
+
+app.use(express.static(path.join(__dirname, 'webpage')));
+app.use(bodyParser.json());
+
+app.use('/players',players);
+app.use('/teams',teams);
+
+app.get('/',(req,res)=>{
+	res.send('Invalid Endpoint');
+});
+
+app.listen(port,()=>{
+	console.log('Server started on port '+port);
+});
